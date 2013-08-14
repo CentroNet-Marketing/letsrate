09e39ecbee7aa1d06783cdfb1253ac3411424f22
Denisse <denisse@centronetmarketing.com>
2013-08-14 10:27:33 -0400

    Seteable display average

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -4,7 +4,8 @@ module Helpers
     cached_average = rateable_obj.average dimension
 
     avg = cached_average ? cached_average.avg : 0
-
+    avg = options[:display_average] == true ? avg : 0
+    
     star = options[:star] || 5
 
     disable_after_rate = options[:disable_after_rate] || true
@@ -12,7 +13,7 @@ module Helpers
     readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
 
     readonly = options[:readonly] unless options[:readonly].nil?
-    
+
     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
